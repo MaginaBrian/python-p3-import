6ae3e86fcbe94e94321c1ab1f2e07580ca830613
MaginaBrian <brianojuk@gmail.com>
2025-05-15 21:56:04 +0300

    Complete imports assignment

---
+++ lib/absolute_package/package1/module2.py
@@ -0,0 +1,4 @@
+from .module1 import function1
+
+def call_function1():
+    function1()
---
+++ lib/absolute_package/package2/module4.py
@@ -0,0 +1,4 @@
+from package2.subpackage1.module3 import function1
+
+def call_function1():
+    function1()
